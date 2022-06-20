fcf8a0deea0010df415bcab69d51b82cb5aaceef
André <a95duran@gmail.com>
2022-06-20 11:06:15 -0500

    Finish adding new functionality and interface

--- script.js
+++ script.js
@@ -1,46 +1,125 @@
+let playerScore = 0;
+let computerScore = 0;
+
+const resultsParagraph = document.querySelector("#results-announcement");
+
 function computerPlay() {
   let cpuChoice = null;
-  let choices = ["Rock", "Paper", "Scissors"];
+  let choices = ["rock", "paper", "scissors"];
 
   cpuChoice = choices[Math.floor(Math.random() * choices.length)];
-
-  console.log(`The computer chooses ${cpuChoice}`);
   return cpuChoice;
 }
 
 function playRound(playerSelection, computerSelection) {
+  let displayMessage = "";
   switch (playerSelection.toLowerCase()) {
     case "rock":
-      if (computerSelection == "Rock") return "It's a tie!";
-      else if (computerSelection == "Scissors")
-        return "You win! Rock beats Scissors";
-      else return "You lose! Paper beats Rock";
+      if (computerSelection == "rock") {
+        displayMessage = `The player chooses ${playerSelection}.
+        \nThe computer plays ${computerSelection}.
+        \nIt's a tie!`;
+      } else if (computerSelection == "scissors") {
+        displayMessage = `The player chooses ${playerSelection}.
+        \nThe computer plays ${computerSelection}.
+        \nYou win! Rock beats scissors`;
+        updateScores("player");
+      } else {
+        displayMessage = `The player chooses ${playerSelection}.
+        \nThe computer plays ${computerSelection}.
+        \nYou lose! Paper beats rock`;
+        updateScores("cpu");
+      }
       break;
     case "paper":
-      if (computerSelection == "Rock") return "You win! Paper beats Rock";
-      else if (computerSelection == "Scissors")
-        return "You lose! Scissors beat Paper";
-      else console.log("It's a tie!");
+      if (computerSelection == "rock") {
+        displayMessage = `The player chooses ${playerSelection}.
+        \nThe computer plays ${computerSelection}.
+        \nYou win! Paper beats rock`;
+        updateScores("player");
+      } else if (computerSelection == "scissors") {
+        displayMessage = `The player chooses ${playerSelection}.
+        \nThe computer plays ${computerSelection}.
+        \nYou lose! Scissors beat Paper`;
+        updateScores("cpu");
+      } else {
+        displayMessage = `The player chooses ${playerSelection}.
+        \nThe computer plays ${computerSelection}.
+        \nIt's a tie!`;
+      }
       break;
     case "scissors":
-      if (computerSelection == "Rock")
-        console.log("You lose! Rock beats Scissors");
-      else if (computerSelection == "Scissors") console.log("It's a tie!");
-      else console.log("You win! Scissors beat Paper");
+      if (computerSelection == "rock") {
+        displayMessage = `The player chooses ${playerSelection}.
+        \nThe computer plays ${computerSelection}.
+        \nYou lose! Rock beats scissors`;
+        updateScores("cpu");
+      } else if (computerSelection == "scissors")
+        displayMessage = `The player chooses ${playerSelection}.
+        \nThe computer plays ${computerSelection}.
+        \nIt's a tie!`;
+      else {
+        displayMessage = `The player chooses ${playerSelection}.
+        \nThe computer plays ${computerSelection}.
+        \nYou win! Scissors beat Paper`;
+        updateScores("player");
+      }
       break;
   }
+
+  return displayMessage;
 }
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt("Please, write down your selection");
-    console.log(
-      "The player chooses " +
-        playerSelection.charAt(0).toUpperCase() +
-        playerSelection.slice(1).toLowerCase()
-    );
-    console.log(playRound(playerSelection, computerPlay()));
+function game(playerSelection) {
+  resultsParagraph.style.color = "aliceblue";
+  /*resultsParagraph.style.borderColor = "red";
+  resultsParagraph.style.borderStyle = "solid";
+  resultsParagraph.style.borderWidth = "5px";*/
+
+  resultsParagraph.textContent = playRound(playerSelection, computerPlay());
+  //resultsContainer.appendChild(resultsParagraph);
+}
+
+function updateScores(winner) {
+  const p_score = document.querySelector("#player-score");
+  const c_score = document.querySelector("#computer-score");
+
+  switch (winner) {
+    case "player":
+      playerScore++;
+      p_score.textContent = playerScore;
+      break;
+    case "cpu":
+      computerScore++;
+      c_score.textContent = computerScore;
+      break;
   }
+
+  if (playerScore >= 5)
+    resultsParagraph.textContent = "You win! Congratulations!";
+  else if (computerScore >= 5)
+    resultsParagraph.textContent = "You lose! Better luck next time!";
 }
 
-game();
+const playerButtons = document.querySelectorAll("button");
+playerButtons.forEach((element) => {
+  element.addEventListener("click", () => {
+    let buttonValue = "";
+    switch (element.id) {
+      case "rock-button":
+        buttonValue = "rock";
+        break;
+      case "paper-button":
+        buttonValue = "paper";
+        break;
+      case "scissors-button":
+        buttonValue = "scissors";
+        break;
+    }
+    if (playerScore >= 5 || computerScore >= 5) return;
+    else {
+      game(buttonValue);
+      updateScores();
+    }
+  });
+});
